bc965b9821f6668e036e7acfe88eb1c821ca72fd
nalodagreat <nalothegreat0@gmail.com>
2025-08-08 11:34:39 +0200

    feat: Add FindClientScreen - Separation of UI from class logic - Making the code more effective

---
+++ Bank System/FindClientScreen.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <iostream>
+#include "Screen.h"
+#include "Person.h"
+#include "BankClient.h"
+#include "InputValidate.h"
+
+class clsFindClientScreen :protected Screen
+{
+
+private:
+    static void printClient(BankClient Client)
+    {
+        cout << "\nClient Card:";
+        cout << "\n___________________";
+        cout << "\nFirstName   : " << Client.getFirstName();
+        cout << "\nLastName    : " << Client.getLastName();
+        cout << "\nEmail       : " << Client.getEmail();
+        cout << "\nPhone       : " << Client.getPhoneNumber();
+        cout << "\nAcc. Number : " << Client.getId();
+        cout << "\nPassword    : " << Client.getPin();
+        cout << "\nBalance     : " << Client.getAccountBalance();
+        cout << "\n___________________\n";
+
+    }
+
+public:
+
+    static void ShowFindClientScreen()
+    {
+
+        drawScreenHeader("\tFind Client Screen");
+
+        string AccountNumber;
+        cout << "\nPlease Enter Account Number: ";
+        AccountNumber = InputValidate::readString();
+        while (!BankClient::isClietExist(AccountNumber));
+        {
+            cout << "\nAccount number is not found, choose another one: ";
+            AccountNumber = InputValidate::readString();
+        }
+
+        BankClient Client1 = BankClient::find(AccountNumber);
+
+        if (!Client1.isEmpty())
+        {
+            cout << "\nClient Found :-)\n";
+        }
+        else
+        {
+            cout << "\nClient Was not Found :-(\n";
+        }
+
+        printClient(Client1);
+
+    }
+
+};
+
