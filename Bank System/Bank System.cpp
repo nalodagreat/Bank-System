ef908668ab07b5089b6fa0a69e5516810ceb5e7e
nalodagreat <nalothegreat0@gmail.com>
2025-08-09 12:57:47 +0200

    refactor:updating the main and calling mainMenue

--- Bank System/Bank System.cpp
+++ Bank System/Bank System.cpp
@@ -1,55 +1,11 @@
-#include "BankClient.h"
-#include "InputValidate.h"
-#include <iostream>
-#include <iomanip>
 #include "MainScreen.h"
-#include "ClientListScreen.h"
-#include "AddNewClientScreen.h"
-using namespace std;
 
 
-void PrintClientBalance(BankClient Client)
-{
 
-    cout << "| " << setw(15) << left << Client.getId();
-    cout << "| " << setw(20) << left << Client.getFirstName();
-    cout << "| " << setw(20) << left << Client.getLastName();
-    cout << "| " << setw(12) << left << Client.getPhoneNumber();
-    cout << "| " << setw(12) << left << Client.getAccountBalance();
-    cout << "_____________________________________________n";
-    cout << "clients total balance:  " << setw(20) << left << BankClient::getTotalBalnceOfallClients();
-}
-void ShowClientsBalnces()
-{
-    vector <BankClient> vClients = BankClient::getClientsList();
-
-    cout << "\n\t\t\t\t\tClient List (" << vClients.size() << ") Client(s).";
-    cout << "\n_______________________________________________________";
-    cout << "_________________________________________\n" << endl;
-
-    cout << "| " << left << setw(15) << "ID";
-    cout << "| " << left << setw(20) << "Client Name";
-    cout << "| " << left << setw(12) << "Balance";
-    cout << "\n_______________________________________________________";
-    cout << "_________________________________________\n" << endl;
-
-    if (vClients.size() == 0)
-        cout << "\t\t\t\tNo Clients Available In the System!";
-    else
-
-        for (BankClient Client : vClients)
-        {
-
-            /*PrintClientBalance(Client);*/
-            cout << endl;
-        }
-
-    cout << "\n_______________________________________________________";
-    cout << "_________________________________________\n" << endl;
-}
 
 int main()
 {
    
-    AddNewClientScreen::addNewClient();
+    MainScreen::showMainMenue();
+    return 0;
 }
