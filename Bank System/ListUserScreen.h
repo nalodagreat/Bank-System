5ac3fc074091fde0fc49542e973694a96b30ec3b
nalodagreat <nalothegreat0@gmail.com>
2025-08-10 21:01:36 +0200

    feat:adding listserScree

---
+++ Bank System/ListUserScreen.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include "Screen.h"
+#include "User.h"
+
+using namespace std;
+
+class ListUsersScreen : protected Screen
+{
+private:
+    static void printUserRecordLine(User user)
+    {
+        cout << setw(8) << left << "" << "| " << setw(12) << left << user.getUserName();
+        cout << "| " << setw(25) << left << user.fullName();
+        cout << "| " << setw(12) << left << user.Phone;
+        cout << "| " << setw(20) << left << user.Email;
+        cout << "| " << setw(10) << left << user.getPassword();
+        cout << "| " << setw(12) << left << user.getPermissions();
+    }
+
+public:
+    static void showUsersList()
+    {
+        vector<User> users = User::getUsersList();
+
+        string title = "\t  User List Screen";
+        string subTitle = "\t    (" + to_string(users.size()) + ") User(s).";
+
+        drawScreenHeader(title, subTitle);
+
+        cout << setw(8) << left << "" << "\n\t_______________________________________________________";
+        cout << "______________________________________________\n" << endl;
+
+        cout << setw(8) << left << "" << "| " << left << setw(12) << "UserName";
+        cout << "| " << left << setw(25) << "Full Name";
+        cout << "| " << left << setw(12) << "Phone";
+        cout << "| " << left << setw(20) << "Email";
+        cout << "| " << left << setw(10) << "Password";
+        cout << "| " << left << setw(12) << "Permissions";
+        cout << setw(8) << left << "" << "\n\t_______________________________________________________";
+        cout << "______________________________________________\n" << endl;
+
+        if (users.empty())
+            cout << "\t\t\t\tNo Users Available In the System!";
+        else
+            for (User& user : users)
+            {
+                printUserRecordLine(user);
+                cout << endl;
+            }
+
+        cout << setw(8) << left << "" << "\n\t_______________________________________________________";
+        cout << "______________________________________________\n" << endl;
+    }
+};
