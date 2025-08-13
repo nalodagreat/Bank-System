75bb7ede98b27548856e003fea016e4d73ea6913
nalodagreat <nalothegreat0@gmail.com>
2025-08-13 22:00:06 +0200

    adding transferLog method

--- Bank System/TransactionScreen.h
+++ Bank System/TransactionScreen.h
@@ -10,13 +10,13 @@ class TransactionScreen : protected Screen
 private:
     enum enTransactionsMenueOptions {
         eDeposit = 1, eWithdraw = 2,
-        eShowTotalBalance = 3,eTransfer = 4,eShowMainMenue = 5
+        eShowTotalBalance = 3,eTransfer = 4,eTransferLog=5,eShowMainMenue = 6
     };
 
     static short readTransactionsMenueOption()
     {
-        cout << setw(37) << left << "" << "Choose what do you want to do? [1 to 5]? ";
-        short Choice = InputValidate::readShortNumberBetween(1, 5, "Enter Number between 1 to 5? ");
+        cout << setw(37) << left << "" << "Choose what do you want to do? [1 to 6]? ";
+        short Choice = InputValidate::readShortNumberBetween(1, 6, "Enter Number between 1 to 6? ");
         return Choice;
     }
 
@@ -42,6 +42,11 @@ private:
     {
          //cout << "\n Transfere Screen will be here.\n";
         TransferScreen::showTransferScreen();
+    }
+    static void showTransferLogScreen()
+    {
+        cout << "\n TransfereLog Screen will be here.\n";
+        
     }
 
     static void goBackToTransactionsMenue()
@@ -87,6 +92,13 @@ private:
             goBackToTransactionsMenue();
             break;
         }
+        case enTransactionsMenueOptions::eTransferLog:
+        {
+            system("cls");
+            showTransferLogScreen();
+            goBackToTransactionsMenue();
+            break;
+        }
 
 
         case enTransactionsMenueOptions::eShowMainMenue:
@@ -111,6 +123,7 @@ public:
         cout << setw(37) << left << "" << "\t[2] Withdraw.\n";
         cout << setw(37) << left << "" << "\t[3] Total Balances.\n";
         cout << setw(37) << left << "" << "\t[4] Transfer.\n";
+        cout << setw(37) << left << "" << "\t[6] Transfer Log.\n";
         cout << setw(37) << left << "" << "\t[5] Main Menue.\n";
         cout << setw(37) << left << "" << "===========================================\n";
 
