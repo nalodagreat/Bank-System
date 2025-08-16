6cefeb6e2c1cfa1cbca1fc0fa774c2430badeac9
nalodagreat <nalothegreat0@gmail.com>
2025-08-16 22:27:43 +0200

    refactor:updating listScreen Ui

--- Bank System/ClientListScreen.h
+++ Bank System/ClientListScreen.h
@@ -1,60 +1,59 @@
 #pragma once
-#include<string>
+#include <string>
 #include <iostream>
+#include <iomanip>
+#include <vector>
 #include "BankClient.h"
 #include "Screen.h"
+
+using namespace std;
+
 class ClientListScreen : protected Screen
 {
 private:
-	static void PrintClientRecordLine(BankClient Client)
-	{
+    static void PrintClientRecordLine(BankClient Client)
+    {
+        cout << "| " << setw(15) << left << Client.getId()
+            << "| " << setw(20) << left << Client.getFirstName()
+            << "| " << setw(20) << left << Client.getLastName()
+            << "| " << setw(12) << left << Client.getPhoneNumber()
+            << "| " << setw(25) << left << Client.getEmail()
+            << "| " << setw(10) << left << Client.getPin()
+            << "| " << setw(12) << left << fixed << setprecision(2) << Client.getAccountBalance()
+            << " |" << endl;
+    }
 
+    static void PrintDivider()
+    {
+        cout << string(126, '_') << endl;  
+    }
 
-
-		cout << "| " << setw(15) << left << Client.getId();
-		cout << "| " << setw(20) << left << Client.getFirstName();
-		cout << "| " << setw(20) << left << Client.getLastName();
-		cout << "| " << setw(12) << left << Client.getPhoneNumber();
-		cout << "| " << setw(20) << left << Client.getEmail();
-		cout << "| " << setw(10) << left << Client.getPin();
-		cout << "| " << setw(12) << left << Client.getAccountBalance();
-	}
 public:
-	static void ShowClientsList()
-	{
+    static void ShowClientsList()
+    {
+        vector<BankClient> vClients = BankClient::getClientsList();
+        string title = "Show Client Screen", subTitle = to_string(vClients.size());
+        drawScreenHeader(title, subTitle);
 
-		vector <BankClient> vClients = BankClient::getClientsList();
-		string title = "show client screen", subTitle = to_string(vClients.size());
-		drawScreenHeader(title, subTitle);
+        cout << "\n\t\t\tClient List (" << vClients.size() << ") Client(s).\n" << endl;
 
-		cout << "\n\t\t\t\t\tClient List (" << vClients.size() << ") Client(s).";
-		cout << "\n_______________________________________________________";
-		cout << "_________________________________________\n" << endl;
-		cout << "| " << left << setw(15) << "ID";
-		cout << "| " << left << setw(20) << "Client Name";
-		cout << "| " << left << setw(20) << "Client SureName";
-		cout << "| " << left << setw(12) << "Phone";
-		cout << "| " << left << setw(20) << "Email";
-		cout << "| " << left << setw(10) << "Pin Code";
-		cout << "| " << left << setw(12) << "Balance";
-		cout << "\n_______________________________________________________";
-		cout << "_________________________________________\n" << endl;
+        PrintDivider();
+        cout << "| " << setw(15) << left << "ID"
+            << "| " << setw(20) << left << "First Name"
+            << "| " << setw(20) << left << "Last Name"
+            << "| " << setw(12) << left << "Phone"
+            << "| " << setw(25) << left << "Email"
+            << "| " << setw(10) << left << "Pin Code"
+            << "| " << setw(12) << left << "Balance"
+            << " |" << endl;
+        PrintDivider();
 
-		if (vClients.size() == 0)
-			cout << "\t\t\t\tNo Clients Available In the System!";
-		else
+        if (vClients.size() == 0)
+            cout << "\t\t\tNo Clients Available in the System!" << endl;
+        else
+            for (const BankClient& Client : vClients)
+                PrintClientRecordLine(Client);
 
-			for (BankClient Client : vClients)
-			{
-
-				PrintClientRecordLine(Client);
-				cout << endl;
-			}
-
-		cout << "\n_______________________________________________________";
-		cout << "_________________________________________\n" << endl;
-
-	}
-	
+        PrintDivider();
+    }
 };
-
