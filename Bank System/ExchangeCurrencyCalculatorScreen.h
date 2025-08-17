8f7afed72997afc8cbb058b57523e29c95972360
nalodagreat <nalothegreat0@gmail.com>
2025-08-17 21:43:29 +0200

    feat:adding currency calculator

---
+++ Bank System/ExchangeCurrencyCalculatorScreen.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include "Screen.h"
+#include "Currency.h"
+#include "InputValidate.h"
+using namespace std;
+class ExchangeCurrencyCalculatorScreen : protected Screen
+{
+private:
+	Currency readCurrency(string message)
+	{
+			
+		cout << message;
+		string currencyCode = InputValidate::readString();
+		Currency currency = Currency::findCurrencyByCode(currencyCode);
+		while (currency.getMode() == Currency::emptyCurrency)
+		{
+			cout << "Currency not found. Please enter a valid currency code: ";
+			currencyCode = InputValidate::readString();
+			currency = Currency::findCurrencyByCode(currencyCode);
+		}
+		return currency;
+	}
+	static double readAmountToExchange()
+	{
+		cout << "Enter the amount you want to exchange: ";		
+		return InputValidate::readDblNumber();
+	}
+	static void printCurrencyExChangeInfo( Currency currency1,Currency currency2 )
+	{
+		double exchangeValue = currency1.calculateExchangeCurrency(currency1, currency2, readAmountToExchange());
+		cout<< "Currency Exchange Details:" << endl;
+		cout << "-----------------" << endl;
+		cout << "From Currency: " << currency1.getCurrencyCode() << " (" << currency1.getCurrencyName() << ")" << endl;
+		cout << "To Currency: " << currency2.getCurrencyCode() << " (" << currency2.getCurrencyName() << ")" << endl;
+		cout << "Exchange Rate: " << fixed << setprecision(4) <<exchangeValue << " " << currency2.getCurrencyCode() << " per 1 " << currency1.getCurrencyCode() << endl;
+		
+	}
+public:
+	void showExchangeCurrencyCalculatorScreen()
+	{
+		drawScreenHeader("Exchange Currency Calculator", "Calculate the exchange rate between two currencies");
+		
+	}
+};
+
